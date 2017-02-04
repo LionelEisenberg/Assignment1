2c4e05336a063c8ed4b4ed8fbc5a3919640d3985
ahe3 <lionel.eisenberg@gmail.com>
2017-02-04 17:51:30 -0500

    got through all steps but 4 and working on fixing crash

---
+++ Assignment1/Assignment1/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  Assignment1
+//
+//  Created by CS Student on 2/4/17.
+//  Copyright © 2017 LionelEisenberg. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var overviewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
