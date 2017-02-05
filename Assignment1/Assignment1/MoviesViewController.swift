b0253a1dd2c6759c2edca69e8370fec2c020e6c4
ahe3 <lionel.eisenberg@gmail.com>
2017-02-05 11:35:44 -0500

    debugging

--- Assignment1/Assignment1/MoviesViewController.swift
+++ Assignment1/Assignment1/MoviesViewController.swift
@@ -21,10 +21,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         tableView.delegate = self
         let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(), for: UIControlEvents.valueChanged)
-        tableView.insertSubview(refreshControl, at: 0)
         // Do any additional setup after loading the view.
         
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+    }
+
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -46,7 +49,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         }
         task.resume()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
