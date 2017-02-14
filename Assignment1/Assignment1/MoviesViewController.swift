c9bbe6011ff45996b67c47df22fafa1f349124e5
ahe3 <lionel.eisenberg@gmail.com>
2017-02-13 22:35:15 -0500

    got navigation bar customised and images now load

--- Assignment1/Assignment1/MoviesViewController.swift
+++ Assignment1/Assignment1/MoviesViewController.swift
@@ -23,7 +23,6 @@ class MoviesViewController:UIViewController, UITableViewDataSource, UITableViewD
     var movies: [NSDictionary]?
     var filteredData: [NSDictionary]?
     var endPoint: String!
-    var offset: Int = 0
     var isMoreDataLoading = false
     
     override func viewDidLoad() {
@@ -38,6 +37,21 @@ class MoviesViewController:UIViewController, UITableViewDataSource, UITableViewD
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
         loadData(refreshControl: refreshControl)
+        
+        self.navigationItem.title = "Movies"
+        if let navigationBar = navigationController?.navigationBar {
+            navigationBar.tintColor = UIColor(red: 0.250, green: 0.25, blue: 1.0, alpha: 0.8)
+            
+            let shadow = NSShadow()
+            shadow.shadowColor = UIColor.gray.withAlphaComponent(0.5)
+            shadow.shadowBlurRadius = 4;
+            navigationBar.titleTextAttributes = [
+                NSFontAttributeName : UIFont.boldSystemFont(ofSize: 22),
+                NSForegroundColorAttributeName : UIColor(red: 0.5, green: 0.15, blue: 0.15, alpha: 0.8),
+                NSShadowAttributeName : shadow
+            ]
+        }
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
     }
     
     
@@ -49,7 +63,7 @@ class MoviesViewController:UIViewController, UITableViewDataSource, UITableViewD
     func loadData(refreshControl: UIRefreshControl) {
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         if let endPoint = endPoint {
-        let url = URL(string: "https://api.themoviedb.org/3/movie/\(endPoint)?api_key=\(apiKey)&offset=\(offset*20)")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/\(endPoint)?api_key=\(apiKey)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         
@@ -95,10 +109,41 @@ class MoviesViewController:UIViewController, UITableViewDataSource, UITableViewD
         let overview = movie["overview"] as! String
         let baseUrl = "https://image.tmdb.org/t/p/w500"
         if let posterPath = movie["poster_path"] as? String {
-            let imageUrl = NSURL(string: baseUrl + posterPath)
-            cell.posterView.setImageWith(imageUrl as! URL)
+            //let imageUrl = NSURL(string: baseUrl + posterPath)
+            let smallImageUrl = "https://image.tmdb.org/t/p/w45" + posterPath
+            let largeImageUrl = "https://image.tmdb.org/t/p/original" + posterPath
+            //cell.posterView.setImageWith(imageUrl as! URL)
+            let smallImageRequest = NSURLRequest(url: NSURL(string: smallImageUrl)! as URL)
+            let largeImageRequest = NSURLRequest(url: NSURL(string: largeImageUrl)! as URL)
+            cell.posterView.setImageWith(smallImageRequest as URLRequest, placeholderImage: nil, success: { (smallImageRequest, smallImageResponse, smallImage) -> Void in
+                cell.posterView.alpha = 0.0
+                cell.posterView.image = smallImage;
+                UIView.animate(withDuration: 0.3, animations: { () -> Void in
+                cell.posterView.alpha = 1.0
+                    
+                }, completion: { (sucess) -> Void in
+                    
+                    // The AFNetworking ImageView Category only allows one request to be sent at a time
+                    // per ImageView. This code must be in the completion block.
+                    cell.posterView.setImageWith(
+                        largeImageRequest as URLRequest,
+                        placeholderImage: smallImage,
+                        success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
+                            
+                            cell.posterView.image = largeImage;
+                            
+                    },
+                        failure: { (request, response, error) -> Void in
+                            // do something for the failure condition of the large image request
+                            // possibly setting the ImageView's image to a default image
+                    })
+                })
+            },
+                                         failure: { (request, response, error) -> Void in
+                                            // do something for the failure condition
+                                            // possibly try to get the large image
+            })
         }
-        
         cell.contentView.alpha = 0
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
@@ -151,17 +196,4 @@ class MoviesViewController:UIViewController, UITableViewDataSource, UITableViewD
  
         
     }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if(!isMoreDataLoading){
-            let scrollViewContentHeight = tableView.contentSize.height
-            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
-            if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging) {
-                isMoreDataLoading = true
-                offset += 1
-                loadData(refreshControl: refreshControl)
-            }
-        }
-    }
-
 }
